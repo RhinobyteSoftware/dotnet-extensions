040d11d13fd18e06b509d5f4494918e26b0007e9
Ryan Thomas <Ryan.Thomas@rhinobyte.com>
2021-02-22 14:11:40 -0600

    Setup - Added placeholder unit tests project

---
+++ tests/Rhinobyte.ReflectionHelpers.UnitTests/MethodBodyParserUnitTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rhinobyte.ReflectionHelpers.UnitTests
+{
+    [TestClass]
+    public class MethodBodyParserUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
